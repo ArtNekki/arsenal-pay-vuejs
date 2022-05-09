b0dff1f43769943b14aaeddaedc888ca535670f6
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-05-09 15:39:19 +1000

    feature: image component updated

--- src/components/base/BaseImage.vue
+++ src/components/base/BaseImage.vue
@@ -3,26 +3,84 @@
 		<source
 			v-if="breakpoints.xl"
 			media="(min-width: 1200px)"
-			:srcset="(src + '--xl.' + ext + ' 1x', src + '--xl@2x.' + ext + ' 2x', src + '--xl@3x.' + ext + ' 3x')"
+			:srcset="
+				'@/assets/images/' +
+				src +
+				'--xl.' +
+				ext +
+				' 1x, @/assets/images/' +
+				src +
+				'--xl@2x.' +
+				ext +
+				' 2x, @/assets/images/' +
+				src +
+				'--xl@3x.' +
+				ext +
+				' 3x'
+			"
 		/>
 		<source
 			v-if="breakpoints.lg"
 			media="(min-width: 992px)"
-			:srcset="(src + '--lg.' + ext + ' 1x', src + '--lg@2x.' + ext + ' 2x', src + '--lg@3x.' + ext + ' 3x')"
+			:srcset="
+				'@/assets/images/' +
+				src +
+				'--lg.' +
+				ext +
+				' 1x, @/assets/images/' +
+				src +
+				'--lg@2x.' +
+				ext +
+				' 2x, @/assets/images/' +
+				src +
+				'--lg@3x.' +
+				ext +
+				' 3x'
+			"
 		/>
 		<source
 			v-if="breakpoints.md"
 			media="(min-width: 768px)"
-			:srcset="(src + '--md.' + ext + ' 1x', src + '--md@2x.' + ext + ' 2x', src + '--md@3x.' + ext + ' 3x')"
+			:srcset="
+				'@/assets/images/' +
+				src +
+				'--md.' +
+				ext +
+				' 1x @/assets/images/' +
+				src +
+				'--md@2x.' +
+				ext +
+				' 2x, @/assets/images/' +
+				src +
+				'--md@3x.' +
+				ext +
+				' 3x'
+			"
 		/>
 		<source
 			v-if="breakpoints.sm"
 			media="(min-width: 576px)"
-			:srcset="(src + '--sm.' + ext + ' 1x', src + '--sm@2x.' + ext + ' 2x', src + '--sm@3x.' + ext + ' 3x')"
+			:srcset="
+				'@/assets/images/' +
+				src +
+				'--sm.' +
+				ext +
+				' 1x, @/assets/images/' +
+				src +
+				'--sm@2x.' +
+				ext +
+				' 2x, @/assets/images/' +
+				src +
+				'--sm@3x.' +
+				ext +
+				' 3x'
+			"
 		/>
 		<img
-			:src="src + '--xs.' + ext"
-			:srcset="src + '--xs@2x.' + ext + ' 2x,' + src + '--xs@3x.' + ext + ' 3x'"
+			:src="'@/assets/images/' + src + '--xs.' + ext"
+			:srcset="
+				'@/assets/images/' + src + '--xs@2x.' + ext + ' 2x, @/assets/images/' + src + '--xs@3x.' + ext + ' 3x'
+			"
 			:width.attr="`${width}px`"
 			:height.attr="`${height}px`"
 			:style="{ maxWidth: maxWidth + 'px' }"
@@ -31,15 +89,20 @@
 	</picture>
 	<img
 		v-else
-		:src="src + '--xs.' + ext"
-		:srcset="src + '--xs@2x.' + ext + ' 2x,' + src + '--xs@3x.' + ext + ' 3x'"
+		:src="require('@/assets/images/' + src + '.' + ext)"
+		:srcset="
+			require('@/assets/images/' + src + '@2x.' + ext) +
+			' 2x,' +
+			require('@/assets/images/' + src + '@3x.' + ext) +
+			' 3x'
+		"
 		:width.attr="`${width}px`"
 		:height.attr="`${height}px`"
 		:style="{ maxWidth: maxWidth + 'px' }"
 		:alt.attr="alt"
 	/>
 </template>
-
+require('@/assets/chat.png')
 <script lang="ts">
 interface Breakpoints {
 	sm?: boolean;
@@ -52,11 +115,14 @@ import { Options, Vue } from "vue-class-component";
 
 @Options({
 	props: {
-		width: Number,
-		height: Number,
-		maxWidth: Number,
+		width: String,
+		height: String,
+		maxWidth: String,
 		src: String,
-		ext: String,
+		ext: {
+			type: String,
+			default: "png",
+		},
 		alt: String,
 		breakpoints: Object,
 	},
